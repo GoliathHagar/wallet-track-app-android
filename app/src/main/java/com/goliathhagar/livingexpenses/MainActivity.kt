7489d726c1107cacec17924512e67cecdca0daa5
GoliathHagar <goliathhagar@outlook.com>
2023-08-12 23:50:17 +0100

    prototype

--- app/src/main/java/com/goliathhagar/livingexpenses/MainActivity.kt
+++ app/src/main/java/com/goliathhagar/livingexpenses/MainActivity.kt
@@ -3,8 +3,17 @@ package com.goliathhagar.livingexpenses
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,17 +22,31 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.goliathhagar.livingexpenses.ui.theme.LivingExpensesTheme
 
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LivingExpensesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Scaffold (
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = { /*TODO*/ }) {
+
+                        }
+                    },
+                    floatingActionButtonPosition = FabPosition.Center,
+                    bottomBar = {
+                        BottomAppBar (){
+                        }
+                    }
+                )
+                {
+                        contentPadding ->
+                    // Screen content
+                    Box(modifier = Modifier.padding(contentPadding)) { /* ... */ }
+
                 }
+
             }
         }
     }
@@ -36,6 +59,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+
 
 @Preview(showBackground = true)
 @Composable
